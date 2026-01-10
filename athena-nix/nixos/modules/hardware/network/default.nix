dc67879edeeff9772ac39a105d1c20173f256b15
Curtis Mitchell <70792951+curtisjm@users.noreply.github.com>
2026-01-09 21:05:25 -0800

    rm old files

--- athena-nix/nixos/modules/hardware/network/default.nix
+++
@@ -1,9 +0,0 @@
-{ lib, config, ... }: {
-  config = lib.mkIf config.athena.baseConfiguration {
-    networking.networkmanager.enable = true;
-    services.vnstat.enable = true;
-    users.users.${config.athena.homeManagerUser} = {
-      extraGroups = [ "networkmanager" ];
-    };
-  };
-}
