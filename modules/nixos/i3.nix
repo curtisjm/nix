c1e7a611a934a582bcf97dae4bf37f3ef0b3a238
Curtis Mitchell <70792951+curtisjm@users.noreply.github.com>
2026-01-20 22:32:21 -0800

    move i3 env vars to system not hm

--- modules/nixos/i3.nix
+++ modules/nixos/i3.nix
@@ -11,6 +11,16 @@
   # Display manager
   services.displayManager.defaultSession = "none+i3";
 
+  # HiDPI scaling for various toolkits
+  environment.sessionVariables = {
+    GDK_SCALE = "2";
+    GDK_DPI_SCALE = "0.5";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "0";
+    QT_SCALE_FACTOR = "2";
+    QT_FONT_DPI = "96";
+    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+  };
+
   environment.systemPackages = with pkgs; [
     rofi           # application launcher
     dunst          # notifications
