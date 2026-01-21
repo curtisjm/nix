675e77f87a9f921eaf2e9f396f5b418be9054558
Curtis Mitchell <=>
2026-01-20 22:21:46 -0800

    asdf

--- home/laptop/default.nix
+++ home/laptop/default.nix
@@ -1,47 +1,48 @@
-{ pkgs, hostConfig, ... }:
 {
-    imports = [
-        ../shared/zsh.nix
-        ../shared/starship.nix
-        ../shared/zoxide.nix
-        ../shared/atuin.nix
-        ../shared/bat.nix
-        ../shared/eza.nix
-        ../shared/fzf.nix
-        ../shared/git.nix
-        ../shared/nvf
-        # ../shared/ghostty.nix
-        ../shared/tmux.nix
-        # ../shared/java.nix
-        ../nixos/hyprland.nix
-        # ../nixos/sway.nix
-        ../nixos/kitty.nix
-        ../nixos/noctalia.nix
-        # ../nixos/waybar.nix
-        # ../nixos/rofi.nix
-    ];
+  pkgs,
+  hostConfig,
+  ...
+}: {
+  imports = [
+    ../shared/zsh.nix
+    ../shared/starship.nix
+    ../shared/zoxide.nix
+    ../shared/atuin.nix
+    ../shared/bat.nix
+    ../shared/eza.nix
+    ../shared/fzf.nix
+    ../shared/git.nix
+    ../shared/nvf
+    ../shared/tmux.nix
+    ../nixos/hyprland.nix
+    ../nixos/kitty.nix
+    ../nixos/noctalia.nix
+    ../nixos/zathura.nix
+    ../nixos/obsidian.nix
+    ../nixos/vesktop.nix
+  ];
 
-    home.username = hostConfig.username;
-    home.homeDirectory = "/home/${hostConfig.username}";
-    home.stateVersion = "25.11";
+  home.username = hostConfig.username;
+  home.homeDirectory = "/home/${hostConfig.username}";
+  home.stateVersion = "25.11";
 
-    home.packages = [
-        # pkgs.git
-        pkgs.tree
-    ];
+  home.packages = [
+    # pkgs.git
+    pkgs.tree
+  ];
 
-    home.sessionVariables = {
-        # EDITOR = "nvim";
-    };
+  home.sessionVariables = {
+    # EDITOR = "nvim";
+  };
 
-    home.file = {
-        # ".config/nvim" = {
-        #     source = ../../config/nvim;
-        #     recursive = true;
-        # };
-    };
+  home.file = {
+    # ".config/nvim" = {
+    #     source = ../../config/nvim;
+    #     recursive = true;
+    # };
+  };
 
-    programs.yazi.enable = true;
+  programs.yazi.enable = true;
 
-    programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 }
--- modules/nixos/packages.nix
+++ modules/nixos/packages.nix
@@ -1,38 +1,41 @@
-{ pkgs, inputs, ... }:
 {
-    environment.systemPackages = with pkgs; [
-        neovim
-        ripgrep
-        fd
-        go
-        python3
-        ruff
-        nil
-        cargo
-        rustc
-        docker
-        docker-compose
-        colima
-        stow
-        uv
-        vscode
-        bat
-        tldr
-        btop
-        rust-analyzer
-        yt-dlp
-        cmake
-        # google-chrome
-        gcc
-        # kitty
-	# claude
-        claude-code
-        # ghostty
-        # alacritty
-	keyd
-	inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
-    	protonvpn-gui
-	obsidian
-	fastfetch
-    ];
+  pkgs,
+  inputs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [
+    neovim
+    ripgrep
+    fd
+    go
+    python3
+    ruff
+    nil
+    cargo
+    rustc
+    docker
+    docker-compose
+    colima
+    stow
+    uv
+    vscode
+    bat
+    tldr
+    btop
+    rust-analyzer
+    yt-dlp
+    cmake
+    # google-chrome
+    gcc
+    # kitty
+    # claude
+    claude-code
+    # ghostty
+    # alacritty
+    keyd
+    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+    protonvpn-gui
+    # obsidian
+    fastfetch
+  ];
 }
