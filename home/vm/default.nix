104b68a75cc2eab8f235634bf3321f09cda76f13
Curtis Mitchell <70792951+curtisjm@users.noreply.github.com>
2026-01-20 22:21:06 -0800

    i3 and vm changes

--- home/vm/default.nix
+++ home/vm/default.nix
@@ -1,50 +1,54 @@
-{ pkgs, hostConfig, ... }:
 {
-    imports = [
-        ../shared/zsh.nix
-        # ../shared/starship.nix
-        ../shared/zoxide.nix
-        ../shared/atuin.nix
-        ../shared/bat.nix
-        ../shared/eza.nix
-        ../shared/fzf.nix
-        ../shared/git.nix
-        # ../shared/ghostty.nix
-        ../shared/tmux.nix
-        ../shared/neovim.nix
-        # ../shared/java.nix
-        ../nixos/i3.nix
-        # ../nixos/sway.nix
-        ../nixos/kitty.nix
-        # ../nixos/alacritty.nix
-        ../nixos/rofi.nix
-        # ../nixos/waybar.nix
-    ];
+  pkgs,
+  hostConfig,
+  ...
+}: {
+  imports = [
+    ../shared/zsh.nix
+    ../shared/starship.nix
+    ../shared/zoxide.nix
+    ../shared/atuin.nix
+    ../shared/bat.nix
+    ../shared/eza.nix
+    ../shared/fzf.nix
+    ../shared/git.nix
+    # ../shared/ghostty.nix
+    ../shared/tmux.nix
+    # ../shared/neovim.nix
+    ../shared/nvf
+    # ../shared/java.nix
+    ../nixos/i3.nix
+    # ../nixos/sway.nix
+    ../nixos/kitty.nix
+    # ../nixos/alacritty.nix
+    ../nixos/rofi.nix
+    # ../nixos/waybar.nix
+  ];
 
-    home.username = hostConfig.username;
-    home.homeDirectory = "/home/${hostConfig.username}";
-    home.stateVersion = "25.11";
+  home.username = hostConfig.username;
+  home.homeDirectory = "/home/${hostConfig.username}";
+  home.stateVersion = "25.11";
 
-    # # Cursor configuration for Wayland/Sway
-    # home.pointerCursor = {
-    #     gtk.enable = true;
-    #     package = pkgs.adwaita-icon-theme;
-    #     name = "Adwaita";
-    #     size = 24;
-    # };
+  # # Cursor configuration for Wayland/Sway
+  # home.pointerCursor = {
+  #     gtk.enable = true;
+  #     package = pkgs.adwaita-icon-theme;
+  #     name = "Adwaita";
+  #     size = 24;
+  # };
 
-    home.packages = [
-        pkgs.tree
-    ];
+  home.packages = [
+    pkgs.tree
+  ];
 
-    home.sessionVariables = {
-        # EDITOR = "nvim";
-    };
+  home.sessionVariables = {
+    # EDITOR = "nvim";
+  };
 
-    home.file = {
-    };
+  home.file = {
+  };
 
-    programs.yazi.enable = true;
+  programs.yazi.enable = true;
 
-    programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 }
