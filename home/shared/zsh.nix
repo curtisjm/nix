0f40a01d1e0b33239bbade2445eb6758b067120b
Curtis Mitchell <70792951+curtisjm@users.noreply.github.com>
2026-04-10 14:15:46 -0700

    disable gastown offer-add prompt in zsh

--- home/shared/zsh.nix
+++ home/shared/zsh.nix
@@ -18,6 +18,7 @@
       eval "$(/opt/homebrew/bin/brew shellenv)"
       export PATH="/Library/TeX/texbin:$PATH"
       export PATH="$HOME/.emacs.d/bin:$PATH"
+      export GASTOWN_DISABLE_OFFER_ADD=1
       cd() {
         z "$@"
         eza -l
